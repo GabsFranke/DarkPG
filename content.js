a749ce3b7ed81f6aacec43e98abe4771eb3ecccd
GabsFranke <frankegabriel@gmail.com>
2021-10-09 00:10:37 -0300

    Pequeña mejora en el codigo

--- content.js
+++ content.js
@@ -1,5 +1,10 @@
 // Target para el observer
 const elDocument = document.documentElement;
+// Configuracion del observer:
+const config = {
+    childList: true,
+    subtree: true
+};
 
 // f para inyectar color blanco al texto generado dinamicamente
 function modifyIframeTextColor() {
@@ -7,33 +12,29 @@ function modifyIframeTextColor() {
     iFrames.forEach(iFrame => {
         if (iFrame.hasAttribute("srcdoc")) {
             iFrame.srcdoc += "<style>*{--common-text:#f1f1f1;--ps-title:#f1f1f1;}</style>";
-            console.log("Inyected: iFrames Mod");
         }
     });
 };
 
 // Instancia de observer
 const iFramesObserver = new MutationObserver(function (mutations) {
-    if (mutations.length > 0) {
-        modifyIframeTextColor()
-    } else {
-        return
-    }
+        mutations.forEach(mutation => {
+            if (mutation.addedNodes[0]) {
+                const nodeList = mutation.addedNodes[0].childNodes;
+                nodeList.forEach(node => {
+                    if (node.className === "basic-block-content") {
+                        modifyIframeTextColor();
+                    }
+                })
+            }
+        });
 });
-
-// Configuracion del observer:
-const config = {
-    childList: true,
-    subtree: true
-};
 
 // Nodos a observar e
 // Invocaciones del observer:
 if (elDocument) {
     iFramesObserver.observe(elDocument, config);
 };
-
-
 
 // Posteriormente, puede detener la observacion
 /* iFramesObserver.disconnect();
